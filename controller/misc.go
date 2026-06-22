89b2353f1dfb8fb9e16dd2829e3cffc554e4aa21
Jeremy Ren <jeremy.ren@datacom.com>
2026-06-22 22:10:54 +1200

    fix(email-verification): email verification not respects the Accept-Language header

--- controller/misc.go
+++ controller/misc.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/constant"
+	"github.com/QuantumNous/new-api/i18n"
 	"github.com/QuantumNous/new-api/logger"
 	"github.com/QuantumNous/new-api/middleware"
 	"github.com/QuantumNous/new-api/model"
@@ -286,10 +287,13 @@ func SendEmailVerification(c *gin.Context) {
 	}
 	code := common.GenerateVerificationCode(6)
 	common.RegisterVerificationCodeWithKey(email, code, common.EmailVerificationPurpose)
-	subject := fmt.Sprintf("%s邮箱验证邮件", common.SystemName)
-	content := fmt.Sprintf("<p>您好，你正在进行%s邮箱验证。</p>"+
-		"<p>您的验证码为: <strong>%s</strong></p>"+
-		"<p>验证码 %d 分钟内有效，如果不是本人操作，请忽略。</p>", common.SystemName, code, common.VerificationValidMinutes)
+	templateData := map[string]any{
+		"SystemName": common.SystemName,
+		"Code":       code,
+		"Minutes":    common.VerificationValidMinutes,
+	}
+	subject := i18n.T(c, i18n.MsgUserEmailVerificationSubject, templateData)
+	content := i18n.T(c, i18n.MsgUserEmailVerificationBody, templateData)
 	err := common.SendEmail(subject, email, content)
 	if err != nil {
 		common.ApiError(c, err)
@@ -315,11 +319,13 @@ func SendPasswordResetEmail(c *gin.Context) {
 		code := common.GenerateVerificationCode(0)
 		common.RegisterVerificationCodeWithKey(email, code, common.PasswordResetPurpose)
 		link := fmt.Sprintf("%s/user/reset?email=%s&token=%s", system_setting.ServerAddress, email, code)
-		subject := fmt.Sprintf("%s密码重置", common.SystemName)
-		content := fmt.Sprintf("<p>您好，你正在进行%s密码重置。</p>"+
-			"<p>点击 <a href='%s'>此处</a> 进行密码重置。</p>"+
-			"<p>如果链接无法点击，请尝试点击下面的链接或将其复制到浏览器中打开：<br> %s </p>"+
-			"<p>重置链接 %d 分钟内有效，如果不是本人操作，请忽略。</p>", common.SystemName, link, link, common.VerificationValidMinutes)
+		templateData := map[string]any{
+			"SystemName": common.SystemName,
+			"Link":       link,
+			"Minutes":    common.VerificationValidMinutes,
+		}
+		subject := i18n.T(c, i18n.MsgUserPasswordResetSubject, templateData)
+		content := i18n.T(c, i18n.MsgUserPasswordResetBody, templateData)
 		err := common.SendEmail(subject, email, content)
 		if err != nil {
 			logger.LogError(c.Request.Context(), fmt.Sprintf("failed to send password reset email to %s: %s", email, err.Error()))
